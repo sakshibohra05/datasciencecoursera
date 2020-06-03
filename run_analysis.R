2885fb07d8eb65ba48056c8832d1421c636e7058
sakshibohra05 <62372080+sakshibohra05@users.noreply.github.com>
2020-06-03 15:55:52 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,43 @@
+#required library
+
 library(dplyr)
 #extracting data into variables
+features <- read.table("features.txt", col.names = c("number","features"))
+activities <- read.table("activity_labels.txt", col.names = c("num", "activity"))
 
-features <- read.table("C:/Users/sakshi/Documents/UCI HAR Dataset/features.txt", col.names = c("n","functions"))
-activities <- read.table("C:/Users/sakshi/Documents/UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
-subject_test <- read.table("C:/Users/sakshi/Documents/UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-x_test <- read.table("C:/Users/sakshi/Documents/UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
-y_test <- read.table("C:/Users/sakshi/Documents/UCI HAR Dataset/test/y_test.txt", col.names = "code")
-subject_train <- read.table("C:/Users/sakshi/Documents/UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("C:/Users/sakshi/Documents/UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
-y_train <- read.table("C:/Users/sakshi/Documents/UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
-#merging data 
+#testing data 
+subject_test <- read.table("test/subject_test.txt", col.names = "subject")
+x_test <- read.table("test/X_test.txt", col.names = features$features)
+y_test <- read.table("test/Y_test.txt", col.names = "num")
+y_test_label <- left_join(y_test, activities, by = "num")
 
-x1 <- rbind(x_train, x_test)
-y1 <- rbind(y_train, y_test)
-Subject_merge <- rbind(subject_train, subject_test)
-Merged <- cbind(Subject_merge, y1, x1)
+tidy_test <- cbind(subject_test, y_test_label, x_test)
+tidy_test <- select(tidy_test, -label)
 
-#extracting required set of data
 
-tidy_set <- Merged %>% select(subject, code, contains("mean"), contains("std"))
+#training data
+subject_train <- read.table("train/subject_train.txt", col.names = "subject")
+x_train <- read.table("train/X_train.txt", col.names = features$features)
+y_train <- read.table("train/Y_train.txt", col.names = "num")
+y_train_label <- left_join(y_train, activities, by = "num")
 
+tidy_train <- cbind(subject_train, y_train_label, x_train)
+tidy_train <- select(tidy_train, -label)
+
+
+# combine test and train data set
+tidy_set <- rbind(tidy_test, tidy_train)
+
+# Extract mean and standard deviation
+tidy_mean_std <- select(tidy_set, contains("mean"), contains("std"))
+
+# Averanging all variable by each subject each activity
+tidy_mean_std$subject <- as.factor(tidy_set$subject)
+tidy_mean_std$activity <- as.factor(tidy_set$activity)
+
+tidy_avg <- tidy_mean_std %>%
+  group_by(subject, activity) %>%
+  summarise_each(funs(mean))
+#creating a file to sotre tidy data
+write.table(tidy_avg, "tidydata.txt", row.name=FALSE)
